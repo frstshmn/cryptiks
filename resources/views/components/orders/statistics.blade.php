43fb8d6e529d674cdede27870f931f73214ee079
frstshmn <korenha.v@gmail.com>
2024-05-08 00:00:26 +0300

    Order history v1.2

--- resources/views/components/orders/statistics.blade.php
+++ resources/views/components/orders/statistics.blade.php
@@ -1,9 +1,15 @@
 <div class="row">
-    <div class="col-12">
-        <div class="card">
+    <div class="col-3">
+        <div class="card h-100">
             <h3 class="fw-bold">Статистика</h3>
-            <div> Середній курс: {{$metadata['average_price']}}</div>
-            <div> Сума {{$metadata['currency_from']}} ({{$metadata['currency_to']}}): {{$metadata['sum_qty']}} ({{$metadata['sum_quoteQty']}})</div>
+            <small>
+                <div>Середній курс: <br>{{$metadata['average_price']}}</div>
+                <br>
+                <div>Сума {{$metadata['currency_from']}} ({{$metadata['currency_to']}}): <br>{{$metadata['sum_qty']}} ({{$metadata['sum_quoteQty']}})</div>
+            </small>
         </div>
+    </div>
+    <div class="col-9">
+        <canvas id="profitChart" width="50%" height="30"></canvas>
     </div>
 </div>
